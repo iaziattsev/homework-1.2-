1b21691fa58e6dad7799cd951a866b27262aa643
iaziattsev <90763434+iaziattsev@users.noreply.github.com>
2022-06-06 11:52:55 +0300

    Add files via upload

---
+++ LinkedList/LinkedList.java
@@ -0,0 +1,13 @@
+package LinkedList;
+
+public interface LinkedList<T> {
+    T get(int index);
+
+    void add(T value);
+
+    void insert(int index, T value);
+
+    void remove(int index);
+
+    int size();
+}
---
+++ LinkedList/LinkedListNodeImpl.java
@@ -0,0 +1,31 @@
+package LinkedList;
+
+public class LinkedListNodeImpl<T> implements LinkedListNode<T> {
+
+    private T value;
+    private LinkedListNodeImpl<T> next;
+
+    public LinkedListNodeImpl(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public T getValue() {
+        return value;
+    }
+
+    @Override
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public LinkedListNodeImpl<T> getNext() {
+        return next;
+    }
+
+    @Override
+    public void setNext(LinkedListNodeImpl<T> next) {
+        this.next = next;
+    }
+}
